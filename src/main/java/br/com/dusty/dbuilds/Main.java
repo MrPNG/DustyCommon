36b27d1fcd03a09eab711bf56d49d644f80ae1c1
MrPingu <luizrodrigo40@gmail.com>
2017-07-03 11:38:30 -0300

    Initial commit

---
+++ src/main/java/br/com/dusty/dbuilds/Main.java
@@ -0,0 +1,27 @@
+package br.com.dusty.dbuilds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public static Main INSTANCE;
+	
+	public Main() {
+		INSTANCE = this;
+	}
+	
+	@Override
+	public void onLoad() {
+	
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+	
+	}
+}
