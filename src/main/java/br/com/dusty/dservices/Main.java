290a06727ca95a348634c0025572df4d6c8374e3
MrPingu <luizrodrigo40@gmail.com>
2017-07-11 01:20:21 -0300

    Undo tests

--- src/main/java/br/com/dusty/dservices/Main.java
+++ src/main/java/br/com/dusty/dservices/Main.java
@@ -4,22 +4,10 @@ import br.com.dusty.dservices.services.shutdown.ShutdownService;
 import br.com.dusty.dservices.services.update.UpdaterService;
 import br.com.dusty.dservices.util.MessageUtils;
 import br.com.dusty.dservices.util.NTPUtils;
-import com.google.gson.Gson;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -36,11 +24,13 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onLoad() {
 		boolean updated = false;
+		
 		try{
 			updated = UpdaterService.updatePlugins();
 		}catch(IOException e){
 			LOGGER.log(Level.SEVERE, MessageUtils.PREFIX + "Couldn't update plugins: " + e.getMessage());
 		}
+		
 		if(updated)
 			Bukkit.shutdown();
 	}
@@ -48,53 +38,15 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		long t = 0;
+		
 		try{
 			t = NTPUtils.getTime();
 		}catch(IOException e){
 			e.printStackTrace();
 		}
+		
 		if(t != 0)
 			ShutdownService.scheduleShutdown();
-		
-		HttpClient httpClient = new DefaultHttpClient();
-		
-		try{
-			HttpGet httpGet = new HttpGet("http://ianszot.duckdns.org/testes/merda.php?query=MrPingu_");
-			
-			HttpResponse httpResponse = httpClient.execute(httpGet);
-			HttpEntity httpEntity = httpResponse.getEntity();
-			
-			String s = EntityUtils.toString(httpEntity);
-			LOGGER.log(Level.WARNING, s);
-			
-			Gson gson = new Gson();
-			Response response = gson.fromJson(s, Response.class);
-			
-			LOGGER.log(Level.WARNING, response.toString());
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		try{
-			HttpPost httpPost = new HttpPost("http://ianszot.duckdns.org/testes/post.php");
-			
-			ArrayList<NameValuePair> nameValuePairs = new ArrayList<>();
-			nameValuePairs.add(new BasicNameValuePair("query", "MrPingu_"));
-			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
-			
-			HttpResponse httpResponse = httpClient.execute(httpPost);
-			HttpEntity httpEntity = httpResponse.getEntity();
-			
-			String s = EntityUtils.toString(httpEntity);
-			LOGGER.log(Level.WARNING, s);
-			
-			Gson gson = new Gson();
-			Response response = gson.fromJson(s, Response.class);
-			
-			LOGGER.log(Level.WARNING, response.toString());
-		}catch(Exception e){
-			e.printStackTrace();
-		}
 	}
 	
 	@Override
